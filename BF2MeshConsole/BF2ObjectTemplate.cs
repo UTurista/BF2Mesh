2ab515ea79491cd630e7e8990a44c9ef9dc4407d
rpoxo <irpoxotaujio@gmail.com>
2018-05-25 18:35:44 +0300

    added fileext detection for mesh loading updated bf2mesh structs visibility added test console app

---
+++ BF2MeshConsole/BF2ObjectTemplate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BF2Mesh;
+using System.Reflection;
+
+namespace BF2MeshConsole.Templates
+{
+    public class BF2ObjectTemplate
+    {
+        private string _Name;
+
+        // common properties
+        public string geometry;
+        public int saveInSeparateFile;
+        public int PreCacheObject;
+
+        public string Name
+        {
+            get { return _Name; }
+        }
+
+        public BF2ObjectTemplate(string name)
+        {
+            this._Name = name;
+        }
+
+        public BF2Object CreateObject()
+        {
+            var newObject = new BF2Object(this);
+            return newObject;
+        }
+
+        public bool Update(BF2ObjectTemplate objectTemplate)
+        {
+            foreach (FieldInfo field in objectTemplate.GetType().GetFields())
+            {
+                var value = field.GetValue(objectTemplate);
+                field.SetValue(this, Convert.ChangeType(value, field.FieldType));
+            }
+
+            return true;
+        }
+    }
+}
